b877c250b2e2ad43fab828d8e3b3dd1830c4db3f
viml kumar <vimalkg@gmail.com>
2025-07-16 16:42:41 +0530

    hscii.vercel.app fbse.vercel.app mck82.vercel.app fb.com/ztrabc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-// import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,12 +11,6 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 import { inglishenglosoftw8asc } from '@/components/hsciifp/hsciifonts'
-
-// const space_grotesk = Space_Grotesk({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-space-grotesk',
-// })
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
